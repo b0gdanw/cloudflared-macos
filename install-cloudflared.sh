3b0028f170b52ace6c8a68faee59b082cec2e1e9
Bogdan <26733455+b0gdanw@users.noreply.github.com>
2020-03-01 18:38:20 +0200

    Update install-cloudflared.sh

--- install-cloudflared.sh
+++ install-cloudflared.sh
@@ -5,17 +5,17 @@ curl -L https://bin.equinox.io/c/VdrWdbjqyF/cloudflared-stable-darwin-amd64.tgz 
 tar -xvf cloudflared-stable-darwin-amd64.tgz 
 
 # Step 2: Installing and configuring cloudflared as a service
-sudo cp ./cloudflared /usr/local/bin/cloudflared
-sudo chmod a+rx /usr/local/bin/cloudflared
+cp ./cloudflared /usr/local/bin/cloudflared
+chmod a+rx /usr/local/bin/cloudflared
 echo 'proxy-dns: true
 no-autoupdate: true
 proxy-dns-upstream:
  - https://1.1.1.1/dns-query
  - https://dns.google/dns-query
  - https://1.0.0.1/dns-query' > config.yml
-sudo mkdir -p /usr/local/etc/cloudflared
-sudo cp ./config.yml /usr/local/etc/cloudflared/config.yml
-sudo /usr/local/bin/cloudflared service install
+mkdir -p /usr/local/etc/cloudflared
+cp ./config.yml /usr/local/etc/cloudflared/config.yml
+/usr/local/bin/cloudflared service install
 
 # Step 3: Removing temporary files and testing configuration
 cd
