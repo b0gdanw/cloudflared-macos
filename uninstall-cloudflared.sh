3449910d90d5377034605c7eb42db7f8f40b331e
Bogdan <bogdan198024@yahoo.com>
2020-03-01 16:48:40 +0200

    Update uninstall-cloudflared.sh

--- uninstall-cloudflared.sh
+++ uninstall-cloudflared.sh
@@ -1,9 +1,10 @@
+# Step 1: Uninstalling cloudflared service and removing files
 sudo cloudflared service uninstall
 sudo rm -f /usr/local/bin/cloudflared
 sudo rm -rf /usr/local/etc/cloudflared
+
+# Step 2: Setting Google DNS for Ethernet and testing
 networksetup -setdnsservers Ethernet Empty
-networksetup -setdnsservers Wi-Fi Empty
 dscacheutil -flushcache
 networksetup -setdnsservers Ethernet 8.8.8.8 8.8.4.4
-networksetup -setdnsservers Wi-Fi 8.8.8.8 8.8.4.4
 nslookup cloudflare.com
