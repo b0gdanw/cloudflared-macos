587081ca120e8ffb6b3581d3bbfca09fb8c9257b
Bogdan <bogdan198024@yahoo.com>
2020-03-01 16:47:40 +0200

    Update install-cloudflared.sh

--- install-cloudflared.sh
+++ install-cloudflared.sh
@@ -1,21 +1,26 @@
-mkdir tmp
-cd tmp
+# Step 1: Creating temporary folder, downloading and unpacking cloudflared tgz
+mkdir ~/tmp
+cd ~/tmp
 curl -L https://bin.equinox.io/c/VdrWdbjqyF/cloudflared-stable-darwin-amd64.tgz -o cloudflared-stable-darwin-amd64.tgz
 tar -xvf cloudflared-stable-darwin-amd64.tgz 
+
+# Step 2: Installing and configuring cloudflared as a service
 sudo cp ./cloudflared /usr/local/bin/cloudflared
 sudo chmod a+rx /usr/local/bin/cloudflared
 echo 'proxy-dns: true
+no-autoupdate: true
 proxy-dns-upstream:
  - https://1.1.1.1/dns-query
+ - https://dns.google/dns-query
  - https://1.0.0.1/dns-query' > config.yml
 sudo mkdir -p /usr/local/etc/cloudflared
 sudo cp ./config.yml /usr/local/etc/cloudflared/config.yml
-sudo cloudflared service install
+sudo /usr/local/bin/cloudflared service install
+
+# Step 3: Removing temporary files and testing configuration
 cd
-rm -r tmp
+rm -rf ~/tmp
 networksetup -setdnsservers Ethernet Empty
-networksetup -setdnsservers Wi-Fi Empty
 dscacheutil -flushcache
 networksetup -setdnsservers Ethernet 127.0.0.1
-networksetup -setdnsservers Wi-Fi 127.0.0.1
-nslookup cloudflare.com
+open https://1.1.1.1/help
